286a1b719f0d163fb50e19605f8e98624b4f6bad
Abel Borit Guitton <abelboritguitton@gmail.com>
2025-05-05 22:41:39 -0500

    [add] iconsList

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -1,45 +1,5 @@
 import logo from "../assets/inventarioslogo.png";
 
-import { CiPalette } from "react-icons/ci";
-import { BsEmojiLaughing } from "react-icons/bs";
-import { RiDeleteBin2Line, RiEditLine } from "react-icons/ri";
-import { IoIosArrowDown } from "react-icons/io";
-import { RiVipCrownFill } from "react-icons/ri";
-import { BiUserCircle } from "react-icons/bi";
-import { RiSettings3Line } from "react-icons/ri";
-import { MdExitToApp } from "react-icons/md";
-import { FcPicture } from "react-icons/fc";
-import { CgMathPlus } from "react-icons/cg";
-import { TbBrandSupabase } from "react-icons/tb";
-import { FaReact } from "react-icons/fa";
-import { BsArrowDown, BsArrowUpShort } from "react-icons/bs";
-import { FaBalanceScale } from "react-icons/fa";
-import { BsGoogle } from "react-icons/bs";
-import { RiCloseLine } from "react-icons/ri";
-import { BiSave } from "react-icons/bi";
-import { BsQuestionCircle } from "react-icons/bs";
-import { HiOutlineChartPie } from "react-icons/hi";
-import { SlGraph } from "react-icons/sl";
-import { BsBarChartLine } from "react-icons/bs";
-import { AiOutlineCalculator } from "react-icons/ai";
-import { BsCalendarCheck } from "react-icons/bs";
-import { IoIosArrowForward } from "react-icons/io";
-import { GrFormPrevious, GrCaretNext } from "react-icons/gr";
-import { MdOutlineBorderAll } from "react-icons/md";
-import { MdAlternateEmail } from "react-icons/md";
-import { RiLockPasswordLine } from "react-icons/ri";
-import { MdDriveFileRenameOutline } from "react-icons/md";
-import { RiStockLine } from "react-icons/ri";
-import { BiBellMinus } from "react-icons/bi";
-import { AiOutlineBarcode } from "react-icons/ai";
-import { DiCodepen } from "react-icons/di";
-import { FaRegMoneyBillAlt } from "react-icons/fa";
-import { CiMoneyBill } from "react-icons/ci";
-import { TbBrandBitbucket } from "react-icons/tb";
-import { MdOutlineCategory } from "react-icons/md";
-import { FaBuilding } from "react-icons/fa";
-import { TbReportAnalytics } from "react-icons/tb";
-
 export const variables = {
   logo: logo,
 
@@ -70,47 +30,4 @@ export const variables = {
   colorSecundario: `#DAC1FF`,
   colorselector: "#BF94FF",
   rgbafondos: "rgba(210, 110, 249, 0.1)",
-
-  paletacolores: CiPalette,
-  emoji: BsEmojiLaughing,
-  agregar: CgMathPlus,
-  flechaabajolarga: BsArrowDown,
-  flechaarribalarga: BsArrowUpShort,
-  balance: FaBalanceScale,
-  iconoreportes: TbReportAnalytics,
-  iconoempresa: FaBuilding,
-  iconocategorias: MdOutlineCategory,
-  iconomarca: TbBrandBitbucket,
-  iconoprecioventa: FaRegMoneyBillAlt,
-  iconopreciocompra: CiMoneyBill,
-  iconocodigointerno: DiCodepen,
-  iconocodigobarras: AiOutlineBarcode,
-  iconostockminimo: BiBellMinus,
-  iconostock: RiStockLine,
-  icononombre: MdDriveFileRenameOutline,
-  iconoemail: MdAlternateEmail,
-  iconopass: RiLockPasswordLine,
-  iconeditarTabla: RiEditLine,
-  iconeliminarTabla: RiDeleteBin2Line,
-  iconoFlechabajo: IoIosArrowDown,
-  iconocorona: RiVipCrownFill,
-  iconoUser: BiUserCircle,
-  iconoSettings: RiSettings3Line,
-  iconoCerrarSesion: MdExitToApp,
-  iconofotovacia: FcPicture,
-  iconosupabase: TbBrandSupabase,
-  iconoreact: FaReact,
-  iconogoogle: BsGoogle,
-  iconocerrar: RiCloseLine,
-  iconoguardar: BiSave,
-  iconoayuda: BsQuestionCircle,
-  iconopie: HiOutlineChartPie,
-  iconolineal: SlGraph,
-  iconobars: BsBarChartLine,
-  iconocalculadora: AiOutlineCalculator,
-  iconocheck: BsCalendarCheck,
-  iconoflechaderecha: IoIosArrowForward,
-  iconoprev: GrFormPrevious,
-  icononext: GrCaretNext,
-  iconotodos: MdOutlineBorderAll,
 };
