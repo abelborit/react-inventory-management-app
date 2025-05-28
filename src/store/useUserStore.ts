8c3fb1209bcd1b405fabf2828f0a9dba7d89022a
Abel Borit Guitton <abelboritguitton@gmail.com>
2025-05-27 23:24:34 -0500

    [update][useUserStore] other functions structure & createUser

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -1,10 +1,12 @@
 import { create } from "zustand";
+import { User } from "@supabase/supabase-js";
 import {
   insertUser,
   getAllUsers,
   getUserById,
   updateUser,
   deleteUser,
+  supabase,
 } from "../supabase";
 import {
   UserInsertProps,
@@ -22,7 +24,7 @@ interface UserStore {
   fetchUserById: (id: number) => Promise<void>;
   createUser: (
     props: Omit<UserInsertProps, "user_type"> & { user_type?: UserType }
-  ) => Promise<UserRecord | null>;
+  ) => Promise<User | null>;
   updateUserById: (
     id: number,
     updates: Partial<UserInsertProps>
@@ -30,7 +32,7 @@ interface UserStore {
   deleteUserById: (id: number) => Promise<boolean>;
 }
 
-export const useUserStore = create<UserStore>((set) => ({
+export const useUserStore = create<UserStore>((set, get) => ({
   users: [],
   currentUser: null,
   loading: false,
@@ -42,11 +44,14 @@ export const useUserStore = create<UserStore>((set) => ({
     try {
       const data = await getAllUsers();
 
-      set({ users: data, loading: false });
+      /* TODO: modificar como en "createUser" */
+      set({ users: data });
     } catch (error: any) {
       console.error("fetchUsers error:", error);
 
-      set({ error: error.message ?? "Failed to fetch users", loading: false });
+      set({ error: error.message ?? "Failed to fetch users" });
+    } finally {
+      set({ loading: false });
     }
   },
 
@@ -56,14 +61,14 @@ export const useUserStore = create<UserStore>((set) => ({
     try {
       const data = await getUserById(id);
 
-      set({ currentUser: data, loading: false });
+      /* TODO: modificar como en "createUser" */
+      set({ currentUser: data });
     } catch (error: any) {
       console.error("fetchUserById error:", error);
 
-      set({
-        error: error.message ?? `Failed to fetch user ${id}`,
-        loading: false,
-      });
+      set({ error: error.message ?? `Failed to fetch user ${id}` });
+    } finally {
+      set({ loading: false });
     }
   },
 
@@ -71,24 +76,37 @@ export const useUserStore = create<UserStore>((set) => ({
     set({ loading: true, error: null });
 
     try {
-      const user = await insertUser({
-        ...props,
-        user_type: props.user_type ?? UserType.Employee,
+      const { data, error } = await supabase.auth.signUp({
+        email: props.email,
+        password: props.password,
       });
 
-      if (user) {
-        set((state) => ({ users: [...state.users, user] }));
+      if (error || !data.user) {
+        throw new Error(error?.message ?? "Error creating user");
       }
 
-      set({ loading: false });
+      await insertUser({
+        ...props,
+        user_type: props.user_type ?? UserType.Employee,
+        id_auth: data.user.id,
+      });
 
-      return user;
+      const updatedUsers = [
+        ...get().users,
+        { ...props, id_auth: data.user.id } as UserRecord,
+      ];
+
+      set({ users: updatedUsers });
+
+      return data.user;
     } catch (error: any) {
       console.error("createUser error:", error);
 
-      set({ error: error.message ?? "Failed to create user", loading: false });
+      set({ error: error.message ?? "Failed to create user" });
 
       return null;
+    } finally {
+      set({ loading: false });
     }
   },
 
@@ -96,28 +114,26 @@ export const useUserStore = create<UserStore>((set) => ({
     set({ loading: true, error: null });
 
     try {
-      const user = await updateUser(id, updates);
+      const updated = await updateUser(id, updates);
 
-      if (user) {
+      /* TODO: modificar como en "createUser" */
+      if (updated) {
         set((state) => ({
           users: state.users.map((userElement) =>
-            userElement.id === id ? user : userElement
+            userElement.id === id ? updated : userElement
           ),
         }));
       }
 
-      set({ loading: false });
-
-      return user;
+      return updated;
     } catch (error: any) {
       console.error("updateUserById error:", error);
 
-      set({
-        error: error.message ?? `Failed to update user ${id}`,
-        loading: false,
-      });
+      set({ error: error.message ?? `Failed to update user ${id}` });
 
       return null;
+    } finally {
+      set({ loading: false });
     }
   },
 
@@ -125,26 +141,23 @@ export const useUserStore = create<UserStore>((set) => ({
     set({ loading: true, error: null });
 
     try {
-      const ok = await deleteUser(id);
+      const wasDeleted = await deleteUser(id);
 
-      if (ok) {
+      if (wasDeleted) {
         set((state) => ({
-          users: state.users.filter((user) => user.id !== id),
+          users: state.users.filter((userElement) => userElement.id !== id),
         }));
       }
 
-      set({ loading: false });
-
-      return ok;
+      return wasDeleted;
     } catch (error: any) {
       console.error("deleteUserById error:", error);
 
-      set({
-        error: error.message ?? `Failed to delete user ${id}`,
-        loading: false,
-      });
+      set({ error: error.message ?? `Failed to delete user ${id}` });
 
       return false;
+    } finally {
+      set({ loading: false });
     }
   },
 }));
