8cf22d2a24378614115e58f2d1d88443a46d638b
Abel Borit Guitton <abelboritguitton@gmail.com>
2025-05-28 23:25:34 -0500

    [add] SVG files

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,28 @@
+/* usando "vite-plugin-svgr" se pueden importar los archivos SVG como componentes de React */
+import ChartLine from "./chart-line.svg?react";
+import ChevronLeft from "./chevron-left.svg?react";
+import ChevronRight from "./chevron-right.svg?react";
+import Cog from "./cog.svg?react";
+import House from "./house.svg?react";
+import LogOut from "./log-out.svg?react";
+import Moon from "./moon.svg?react";
+import ScrollText from "./scroll-text.svg?react";
+import Sun from "./sun.svg?react";
+import Crown from "./crown.svg";
+import UserRound from "./user-round.svg";
+import ShieldCheck from "./shield-check.svg";
+
+export {
+  ChartLine,
+  ChevronLeft,
+  ChevronRight,
+  Cog,
+  House,
+  LogOut,
+  Moon,
+  ScrollText,
+  Sun,
+  Crown,
+  UserRound,
+  ShieldCheck,
+};
