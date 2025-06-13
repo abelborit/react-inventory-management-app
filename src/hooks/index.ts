f4fa4ec6154eb03823c9debda013177f2c258a4f
Abel Borit Guitton <abelboritguitton@gmail.com>
2025-06-12 22:41:00 -0500

    [add] useClickOutside & useEscapeKey

---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+export { useDeviceType } from "./useDeviceType";
+export { useErrorGenericHandler } from "./useErrorGenericHandler";
+export { useSupabaseErrorHandler } from "./useSupabaseErrorHandler";
+export { useEscapeKey } from "./useEscapeKey";
+export { useClickOutside } from "./useClickOutside";
